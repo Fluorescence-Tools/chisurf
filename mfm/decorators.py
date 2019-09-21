db84843d0cad51da369396f648ef448d00c1bab3
tpeulen <thomas.otavio.peulen@gmail.com>
2019-09-20 17:39:25 -0700

    Refactoring and added tests

--- mfm/decorators.py
+++ mfm/decorators.py
@@ -64,3 +64,21 @@
 #         return RegisteredClass
 #
 #     return class_rebuilder
+
+
+def set_module(module):
+    """Decorator for overriding __module__ on a function or class.
+
+    Example usage::
+
+        @set_module('numpy')
+        def example():
+            pass
+
+        assert example.__module__ == 'numpy'
+    """
+    def decorator(func):
+        if module is not None:
+            func.__module__ = module
+        return func
+    return decorator
