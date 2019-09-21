e13ebbc5d3c4fa3d28be087b1c665717ddf29af5
tpeulen <thomas.otavio.peulen@gmail.com>
2019-09-20 17:35:25 -0700

    Refactoring and additional tests
    * added additional tests for mfm.fluorescence.convolution
    * renamed mfm.fitting.fitting_widgets to mfm.fitting.widgets

--- mfm/decorators.py
+++ mfm/decorators.py
@@ -64,21 +64,3 @@
 #         return RegisteredClass
 #
 #     return class_rebuilder
-
-
-def set_module(module):
-    """Decorator for overriding __module__ on a function or class.
-
-    Example usage::
-
-        @set_module('numpy')
-        def example():
-            pass
-
-        assert example.__module__ == 'numpy'
-    """
-    def decorator(func):
-        if module is not None:
-            func.__module__ = module
-        return func
-    return decorator
--- test/test_io.py
+++ test/test_io.py
@@ -2,13 +2,10 @@ import utils
 import os
 import unittest
 
-
 TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 utils.set_search_paths(TOPDIR)
 
 import mfm.io
-import tempfile
-import numpy as np
 
 
 class Tests(unittest.TestCase):
@@ -22,37 +19,6 @@ class Tests(unittest.TestCase):
         s = mfm.io.pdb.fetch_pdb_string(pdb_id)
         mfm.io.pdb.parse_string_pdb(s)
 
-    def test_ascii(self):
-        x = np.linspace(
-            start=0,
-            stop=2 * np.pi,
-            num=100
-        )
-        y = np.sin(x)
-        file = tempfile.NamedTemporaryFile(suffix='.txt')
-        mfm.io.ascii.save_xy(
-            filename=file.name,
-            x=x,
-            y=y,
-            fmt="%f\t%f\n"
-        )
-        x2, y2 = mfm.io.ascii.load_xy(
-            filename=file.name,
-            usecols=(0, 1),
-            delimiter="\t"
-        )
-        self.assertEqual(
-            np.allclose(
-                x, x2
-            ),
-            True
-        )
-        self.assertEqual(
-            np.allclose(
-                y, y2
-            ),
-            True
-        )
 
 if __name__ == '__main__':
     unittest.main()
