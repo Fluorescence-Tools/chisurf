6b2d2d3608150e8f3bfbc673273d6a7a9e4ab702
tpeulen <thomas.otavio.peulen@gmail.com>
2019-10-19 19:01:19 -0700

    Fixed import

--- chisurf/fluorescence/__init__.py
+++ chisurf/fluorescence/__init__.py
@@ -2,10 +2,3 @@
 Fluorescence
 
 """
-import chisurf.fluorescence.anisotropy
-import chisurf.fluorescence.fcs
-import chisurf.fluorescence.intensity
-import chisurf.fluorescence.tcspc
-import chisurf.fluorescence.general
-import chisurf.fluorescence.av
-import chisurf.fluorescence.pda
