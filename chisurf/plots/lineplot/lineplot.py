f62de24b448b14dc5cb13d03c8eaec6cf89a650d
tpeulen <thomas.otavio.peulen@gmail.com>
2019-10-28 00:19:15 -0700

    Fixes
    * lineplot region selector

--- chisurf/plots/lineplot/lineplot.py
+++ chisurf/plots/lineplot/lineplot.py
@@ -403,30 +403,36 @@ class LinePlot(plotbase.Plot):
             *args,
             **kwargs
     ):
+        # Get parameters from plot-control
         plt_ctrl = self.pltControl
+        data_log_y = plt_ctrl.data_is_log_y
+        data_log_x = plt_ctrl.data_is_log_x
+        res_log_y = plt_ctrl.res_is_log_y
+        use_reference = self.pltControl.use_reference
+        # # Reference-function
+        # if use_reference:
+        #     reference = self.fit.model.reference
+        #     if reference is None:
+        #         reference = np.ones_like(data_y)
+        #         print("WARNING: no reference curve provided by the model.")
+        #     model_y /= reference[self.fit.xmin:self.fit.xmax]
+        #     data_y /= reference
+        #     mm = max(model_y)
+        #     irf_y *= mm / max(irf_y)
 
         y_shift = plt_ctrl.y_shift
         x_shift = plt_ctrl.x_shift
 
         curves = self.fit.get_curves()
         for curve_key in curves.keys():
-            try:
-                curve = curves[curve_key]
-                y = curve.y
-                if self.pltControl.is_density:
-                    y = curve.y / np.diff(curve.x)
-                self.lines[curve_key].setData(
-                        x=curve.x + x_shift,
-                        y=y
-                    )
-            except:
-                print("Skipping plot %s" % curve_key)
-
-        # Get parameters from plot-control
-        data_log_y = plt_ctrl.data_is_log_y
-        data_log_x = plt_ctrl.data_is_log_x
-        res_log_y = plt_ctrl.res_is_log_y
-        use_reference = self.pltControl.use_reference
+            curve = curves[curve_key]
+            y = curve.y
+            if self.pltControl.is_density:
+                y = curve.y / np.diff(curve.x)
+            self.lines[curve_key].setData(
+                    x=curve.x + x_shift,
+                    y=y
+                )
 
         #
         # model_y = np.copy(model.y) + y_shift
@@ -447,41 +453,6 @@ class LinePlot(plotbase.Plot):
             )
         )
 
-        # # Reference-function
-        # if use_reference:
-        #     reference = self.fit.model.reference
-        #     if reference is None:
-        #         reference = np.ones_like(data_y)
-        #         print("WARNING: no reference curve provided by the model.")
-        #     model_y /= reference[self.fit.xmin:self.fit.xmax]
-        #     data_y /= reference
-        #     mm = max(model_y)
-        #     irf_y *= mm / max(irf_y)
-        #
-        # if not only_fit_range:
-        #     idx = np.where(
-        #         data_y > plt_ctrl.ymin
-        #     )[0] if data_log_y else list(range(len(data_x)))
-        #
-        #     if plt_ctrl.plot_ftt:
-        #         self.data_curve.setData(
-        #             x=data_x[idx],
-        #             y=data_y[idx] * data_x[idx]
-        #         )
-        #     else:
-        #         self.data_curve.setData(
-        #             x=data_x[idx],
-        #             y=data_y[idx]
-        #         )
-        #     if self.plot_irf:
-        #         idx = np.where(
-        #             irf_y > plt_ctrl.ymin
-        #         )[0] if data_log_y else list(range(len(irf_x)))
-        #         self.irf_curve.setData(
-        #             x=irf_x[idx],
-        #             y=irf_y[idx]
-        #         )
-        #
         # Set log-scales
         self.res_plot.setLogMode(
             x=data_log_x,
@@ -496,46 +467,14 @@ class LinePlot(plotbase.Plot):
             y=data_log_y
         )
 
-        # data = curves['data']
-        # lb_min, ub_max = data.x[0], data.x[-1]
-        # lb, ub = data.x[self.fit.xmin], data.x[self.fit.xmax]
-        # self.region.setBounds((lb_min, ub_max))
-        # self.region.setRegion((lb, ub))
-
-        #
-        # # Update the Model-lines (Model, wres, acorr)
-        # idx = np.where(
-        #     model_y > 0.0
-        # )[0] if data_log_y else list(range(len(model_x)))
-        # if plt_ctrl.plot_ftt:
-        #     self.fit_curve.setData(
-        #         x=model_x[idx],
-        #         y=model_y[idx] * model_x[idx]
-        #     )
-        # else:
-        #     self.fit_curve.setData(
-        #         x=model_x[idx],
-        #         y=model_y[idx]
-        #     )
-        #
-        # self.residuals_plot.clear()
-        # self.auto_corr_plot.clear()
-        #
-        # colors = chisurf.settings.gui['plot']['colors']
-        # lw = chisurf.settings.gui['plot']['line_width']
-        #
-        # self.residuals_plot.plot(
-        #     x=wres.x,
-        #     y=wres.y,
-        #     pen=pg.mkPen(colors['residuals'], width=lw),
-        #     name='residues'
-        # )
-        # self.auto_corr_plot.plot(
-        #     x=wres.x[1:],
-        #     y=chisurf.math.signal.autocorr(wres.y)[1:],
-        #     pen=pg.mkPen(colors['auto_corr'], width=lw),
-        #     name='residues'
-        # )
-        # self.data_x = data_x
-        # self.data_y = data_y
-        #
+        # update region selector
+        data = curves['data']
+        lb_min, ub_max = data.x[0], data.x[-1]
+        lb, ub = data.x[self.fit.xmin], data.x[self.fit.xmax]
+        if data_log_x:
+            lb = np.log10(lb)
+            ub = np.log10(ub)
+            lb_min = np.log10(lb_min)
+            ub_max = np.log10(ub_max)
+        self.region.setBounds((lb_min, ub_max))
+        self.region.setRegion((lb, ub))
