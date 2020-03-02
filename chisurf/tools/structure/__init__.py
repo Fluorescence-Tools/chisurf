9a176b259c92315ae92f5c3ee38d3125c85d914c
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-02 10:21:52 -0800

    Improved imports

--- chisurf/tools/structure/__init__.py
+++ chisurf/tools/structure/__init__.py
@@ -1,9 +1,8 @@
-from chisurf.tools.structure import align_trajectory
-from chisurf.tools.structure import convert_trajectories
-from chisurf.tools.structure import fret_screening
-from chisurf.tools.structure import fret_trajectory
-from chisurf.tools.structure import join_trajectories
-from chisurf.tools.structure import potential_energy
-from chisurf.tools.structure import remove_clashed_frames
-from chisurf.tools.structure import save_topology
-from chisurf.tools.structure import create_av_json
+import chisurf.tools.structure.align_trajectory
+import chisurf.tools.structure.convert_trajectory
+import chisurf.tools.structure.fret_trajectory
+import chisurf.tools.structure.join_trajectories
+import chisurf.tools.structure.remove_clashed_frames
+import chisurf.tools.structure.save_topology
+import chisurf.tools.structure.create_av_json
+import chisurf.tools.structure.rotate_translate_trajectory
