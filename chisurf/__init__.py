07a9416cbcfbaed0605a72042c50d8d438d0a6f9
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-13 17:02:39 -0700

    Fixed issue with the dmg-build
    The build-dmg uses the ouput of
    
    python -c "import chisurf; import pathlib; print(pathlib.Path(chisurf.__file__).parent.absolute())"
    
    to determine the path of the python library. The warning was outputed to the stdout.
    Hence, when a

--- chisurf/__init__.py
+++ chisurf/__init__.py
@@ -10,7 +10,10 @@ try:
     else:
         import typing_extensions as typing
 except ModuleNotFoundError:
-    print("WARNING typing_extensions not found")
+    print(
+        "WARNING typing_extensions not found",
+        file=sys.stderr
+    )
     typing = None
 
 import chisurf.settings
@@ -43,35 +46,5 @@ logging.basicConfig(
     filename=settings.session_log,
     level=logging.DEBUG
 )
-
-
-def c(
-        t,
-        st: str,
-        parameters: typing.List
-):
-    """This function facilitates the connection of events to the mfm-command
-    line. Whenever the qt-event is called the string passed as argument is
-    executed at the mfm-commandline with the provided parameters.
-    Here the parameters are either callable functions of strings.
-
-    Example
-    -------
-    >>> import chisurf
-    >>> chisurf.c(self.checkBox.stateChanged, models, self.checkBox.isChecked)
-    >>> cs.current_fit.model.update_rmsd=True
-
-    :param t: The signal of the qt-widget
-    :param st: The string passed to the mfm-commandline
-    :param parameters: the parameters passed to the commandline string
-    """
-    if isinstance(parameters, typing.Iterable):
-        t.connect(
-            lambda: run(
-                st % [p if isinstance(p, str) else p() for p in parameters]
-            )
-        )
-    else:
-        t.connect(lambda: run(st % parameters()))
 
 
