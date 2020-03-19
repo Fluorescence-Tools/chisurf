f1249d8ac9e1254383976391e577d25672d8f695
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-19 16:30:45 -0700

    Build fix

--- build_tools/win/build-setup.bat
+++ build_tools/win/build-setup.bat
@@ -4,16 +4,18 @@ set "SCRIPT_PATH=%~dp0"
 set "APP_PATH=..\..\dist\win"
 set "SOURCE_PATH=..\..\"
 set "CONDA_RECIPE_FOLDER=..\..\conda-recipe"
-call conda install -y conda-build jinja2 pysftp
+
+rem Assume that conda-build jinja2 pysftp are already installed
+rem call conda install -y conda-build jinja2 pysftp
 rem call conda build %CONDA_RECIPE_FOLDER%
 
 md %DIST_PATH%
 md %APP_PATH%
-call conda create -y --prefix %APP_PATH% chisurf python=3.7.3 jinja2 -c local --force
+call conda create -y --prefix %APP_PATH% chisurf python=3.7.3 -c local --force
 call conda activate %APP_PATH%
 
 REM write setup.iss
-call conda activate %APP_PATH%
+rem call conda activate %APP_PATH%
 python make_inno_setup.py
 call conda deactivate
 call conda activate base
