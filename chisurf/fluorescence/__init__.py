2a6c07087fdff3a2aae2d1a6bba47e5bbbd81940
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-18 17:15:22 -0700

    Extended docstring

--- chisurf/fluorescence/__init__.py
+++ chisurf/fluorescence/__init__.py
@@ -2,3 +2,12 @@
 Fluorescence
 
 """
+import chisurf.fluorescence.general
+import chisurf.fluorescence.intensity
+import chisurf.fluorescence.anisotropy
+import chisurf.fluorescence.av
+import chisurf.fluorescence.fcs
+import chisurf.fluorescence.fret
+import chisurf.fluorescence.pda
+import chisurf.fluorescence.simulation
+import chisurf.fluorescence.tcspc
