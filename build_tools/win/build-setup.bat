24f1dea94aba490255607919362966564f7fdb00
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-19 22:48:21 -0700

    Build fix

--- build_tools/win/build-setup.bat
+++ build_tools/win/build-setup.bat
@@ -12,13 +12,13 @@ rem call conda build %CONDA_RECIPE_FOLDER%
 md %DIST_PATH%
 md %APP_PATH%
 call conda create -y --prefix %APP_PATH% chisurf python=3.7.3 -c local --force
-call conda activate %APP_PATH%
+rem call conda activate %APP_PATH%
 
 REM write setup.iss
 rem call conda activate %APP_PATH%
 python make_inno_setup.py
-call conda deactivate
-call conda activate base
+rem call conda deactivate
+rem call conda activate base
 
 REM Create an Installer with Inno Setup
 "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc setup.iss
