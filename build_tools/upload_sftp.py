b974780557d3446489253c5d8f244e7cd99556e2
tpeulen <thomas.otavio.peulen@gmail.com>
2020-03-19 23:27:21 -0700

    Build fix

--- build_tools/upload_sftp.py
+++ build_tools/upload_sftp.py
@@ -5,9 +5,13 @@ import glob
 import argparse
 import os
 
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
 hostname = "ssh.fret.at"
 username = "fret.at"
 password = os.getenv('SFTP_PASSWD')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -37,13 +41,19 @@ if __name__ == "__main__":
     file_pattern = args.file_pattern
     remote_file = args.remote_file
     local_path = glob.glob(file_pattern)[0]
+    print("Uploading file: ", local_path)
     with pysftp.Connection(
             host=hostname,
             username=username,
-            password=password
+            password=password,
+            cnopts=cnopts
     ) as sftp:
         print("Connection established ... ")
         print("Uploading: %s" % local_path)
-        sftp.remove(remote_file)
+        try:
+            sftp.remove(remote_file)
+            print("File already on server. Deleting.")
+        except FileNotFoundError:
+            print("File not on server. Not necessary to delete.")
         sftp.put(local_path, remote_file)
 
