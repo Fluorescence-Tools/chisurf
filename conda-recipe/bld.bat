1a9437f1121f8ff39364e2ac8921ef4a05d1a9c6
tpeulen <thomas.otavio.peulen@gmail.com>
2020-09-09 19:47:36 -0700

    Added new splashscreen and fixed resource reference

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,2 +1,3 @@
+pyrcc5 chisurf/gui/resources/resource.qrc -o chisurf/gui/resources/resource.py
 "%PYTHON%" setup.py build_ext --force --inplace
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
