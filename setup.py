49b022f61aa2faa97fede93da08a515bfc3b394a
tpeulen <thomas.otavio.peulen@gmail.com>
2020-09-14 21:11:23 -0700

    Code clean up
    Created separate file to define entry points

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-
 try:
     import numpy as np
 except ImportError:
     np = None
-import sys
 import platform
 from setuptools import setup, find_packages, Extension
 try:
@@ -76,32 +74,18 @@ def get_extensions():
         return []
 
 
-gui_scripts = {
-    "chisurf": "chisurf.__main__:main",
-    "csg_kappa2distribution": "chisurf.gui.tools.kappa2_distribution.__main__:main",
-    "csg_fps_json_edit": "chisurf.gui.tools.structure.create_av_json.__main__:main",
-    "csg_calculator": "chisurf.gui.tools.fret.calculator.__main__:main",
-    "csg_f_test": "chisurf.gui.tools.f_test.__main__:main",
-    "csg_tttr_clsm_pixel_select": "chisurf.gui.tools.tttr.clsm_pixel_select.__main__:main",
-    "csg_tttr_convert": "chisurf.gui.tools.tttr.convert.__main__:main",
-    "csg_tttr_correlator": "chisurf.gui.tools.tttr.correlate.__main__:main",
-    "csg_tttr_decay_histogram": "chisurf.gui.tools.tttr.decay.__main__:main",
-    "csg_traj_align": "chisurf.gui.tools.structure.align_trajectory.__main__:main",
-    "csg_traj_convert": "chisurf.gui.tools.structure.convert_trajectory.__main__:main",
-    "csg_traj_fret_analysis": "chisurf.gui.tools.structure.fret_trajectory.__main__:main",
-    "csg_traj_join": "chisurf.gui.tools.structure.join_trajectories.__main__:main",
-    "csg_traj_energy_calculator": "chisurf.gui.tools.structure.potential_energy.__main__:main",
-    "csg_traj_remove_clashed_frames": "chisurf.gui.tools.structure.remove_clashed_frames.__main__:main",
-    "csg_traj_rotate_translate": "chisurf.gui.tools.structure.rotate_translate_trajectory.__main__:main",
-    "csg_traj_save_topology": "chisurf.gui.tools.structure.save_topology.__main__:main"
-}
+def dict_from_txt(fn):
+    d = {}
+    with open(fn) as f:
+        for line in f:
+            (key, val) = line.split()
+            d[str(key)] = val
+    return d
 
-console_scripts = {
-    "csc_tttr_decay_histogram": "chisurf.cmd_tools.tttr_decay_histogram:main",
-    "csc_tttr_correlate": "chisurf.cmd_tools.tttr_correlate:main",
-    "csc_fcs_convert": "chisurf.cmd_tools.fcs_convert:main",
-    "csc_protein_mc_fret": "csc_protein_mc_fret.cmd_tools.fcs_convert:main"
-}
+
+gui_scripts = dict_from_txt("./chisurf/entry_points/gui.txt")
+console_scripts = dict_from_txt("./chisurf/entry_points/cmd.txt")
+
 
 metadata = dict(
     name=NAME,
@@ -140,30 +124,15 @@ metadata = dict(
         ]
     },
     install_requires=[
-        'PyQt5',
-        'qtpy',
-        'cython',
-        'sip',
-        'pyqtgraph',
-        'python-slugify',
-        'numba',
-        'numpy',
-        'numexpr',
-        'scipy',
+        'PyQt5', 'qtpy', 'sip', 'pyqtgraph', 'qtconsole',
+        'numba', 'numpy', 'numexpr', 'scipy', 'pyopencl',
+        'cython', 'python-slugify', 'deprecation',
         'emcee',
-        'PyYAML',
+        'PyYAML', 'typing-extensions',
         'tables',
-        'matplotlib',
-        'python-docx',
-        'deprecation',
-        'pyopencl',
-        'opencv-python',
-        'mrcfile',
-        'qtconsole',
-        'ipython',
-        'python-docx',
-        'mdtraj',
-        'typing-extensions'
+        'matplotlib', 'python-docx',
+        'mrcfile', 'mdtraj', 'opencv-python',
+        'ipython'
     ],
     setup_requires=[
         "cython",
