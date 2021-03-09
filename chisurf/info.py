2bc34d28568e604278406927d7d97be8df70e96f
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-03-09 15:10:08 +0100

    Moved info to separate file

---
+++ chisurf/info.py
@@ -0,0 +1,16 @@
+__name__ = "chisurf"
+__author__ = "Thomas-Otavio Peulen"
+__version__ = "20.3.9"
+__copyright__ = "Copyright (C) 2020 Thomas-Otavio Peulen"
+__credits__ = ["Thomas-Otavio Peulen"]
+__maintainer__ = "Thomas-Otavio Peulen"
+__email__ = "thomas@peulen.xyz"
+__url__ = "https://gitlab.peulen.xyz/chisurf/"
+__license__ = 'GPL2.1'
+__status__ = "Dev"
+__description__ = """ChiSurf: an interactive global analysis platform for fluorescence data."""
+__app_id__ = "F25DCFFA-1234-4643-BC4F-2C3A20495937"
+LONG_DESCRIPTION = """ChiSurf: an interactive global analysis platform for fluorescence \
+data."""
+help_url = 'https://github.com/Fluorescence-Tools/chisurf/wiki'
+update_url = 'https://github.com/Fluorescence-Tools/chisurf/releases'
