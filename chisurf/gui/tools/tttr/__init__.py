5ef1415f0819aa69b2b29103cb32b39938c096dc
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-03-09 14:13:14 +0100

    Fixed dependencies
    clsmview and ndxplorer are now separate packages

--- chisurf/gui/tools/tttr/__init__.py
+++ chisurf/gui/tools/tttr/__init__.py
@@ -1,4 +1,3 @@
 from chisurf.gui.tools.tttr import convert
-from chisurf.gui.tools.tttr import clsm_pixel_select
 from chisurf.gui.tools.tttr import decay
 from chisurf.gui.tools.tttr import correlate
