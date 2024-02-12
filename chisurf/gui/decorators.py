d94d4035e53bec0050edfab34aace0b6de200b12
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-12 23:04:15 +0100

    add lineEdit_dragFile_injector decorator

--- chisurf/gui/decorators.py
+++ chisurf/gui/decorators.py
@@ -1,5 +1,8 @@
 import inspect
 import os
+import pathlib
+
+import chisurf
 from chisurf import typing
 
 from qtpy import QtWidgets, uic
@@ -8,18 +11,14 @@ from qtpy import QtWidgets, uic
 class init_with_ui(object):
     """
     This is a decorator for __init__ methods of QtWidget objects.
-    The decorator accepts a ui_filename, calls the super class of
+    The decorator accepts an ui_filename, calls the super class of
     the object, and initializes the ui file to a target specified
     by the target option. If no target is provided the ui file
     is initialized into the object of the __init__ function.
 
     """
 
-    def __init__(
-            self,
-            ui_filename: str,
-            path: str = None
-    ):
+    def __init__(self, ui_filename: str, path: str = None):
         """
 
         :param ui_filename: The filename (without path) of the ui file.
@@ -30,16 +29,9 @@ class init_with_ui(object):
         self.ui_filename = ui_filename
         self.path = path
 
-    def __call__(
-            self,
-            f: typing.Callable
-    ):
+    def __call__(self, f: typing.Callable):
 
-        def load_ui(
-                target: QtWidgets.QWidget,
-                ui_filename: str,
-                path: str
-        ):
+        def load_ui(target: QtWidgets.QWidget, ui_filename: str, path: str):
             uic.loadUi(
                 os.path.join(
                     path,
@@ -48,11 +40,7 @@ class init_with_ui(object):
                 target
             )
 
-        def wrapped(
-                cls: QtWidgets.QWidget,
-                *args,
-                **kwargs
-        ):
+        def wrapped(cls: QtWidgets.QWidget, *args, **kwargs):
             if self.path is None:
                 path = os.path.dirname(
                     inspect.getfile(
@@ -78,3 +66,50 @@ class init_with_ui(object):
             f(cls, *args, **kwargs)
 
         return wrapped
+
+
+
+# reference taken from : http://stackoverflow.com/questions/11872141/drag-a-file-into-qtgui-qlineedit-to-set-url-text
+# https://stackoverflow.com/questions/11872141/drag-a-file-into-qtgui-qlineedit-to-set-url-text
+class lineEdit_dragFile_injector():
+    def __init__(self, lineEdit, auto_inject=True, call=None, target: typing.List[pathlib.Path] = None):
+        self.lineEdit = lineEdit
+        self.call = call
+        self.target = target
+        if auto_inject:
+            self.inject_dragFile()
+
+    def inject_dragFile( self ):
+        self.lineEdit.setDragEnabled(True)
+        self.lineEdit.dragEnterEvent = self._dragEnterEvent
+        self.lineEdit.dragMoveEvent = self._dragMoveEvent
+        self.lineEdit.dropEvent = self._dropEvent
+
+    def _dragEnterEvent( self, event ):
+        data = event.mimeData()
+        urls = data.urls()
+        if urls and urls[0].scheme() == 'file':
+            event.acceptProposedAction()
+
+    def _dragMoveEvent( self, event ):
+        data = event.mimeData()
+        urls = data.urls()
+        if urls and urls[0].scheme() == 'file':
+            event.acceptProposedAction()
+
+    def _dropEvent( self, event):
+        data = event.mimeData()
+        url = data.urls()[0]
+        if isinstance(self.target, list):
+            for url in data.urls():
+                if url.scheme() == 'file':
+                    filepath = str(url.toLocalFile())
+                    filepath = pathlib.Path(filepath).as_posix()
+                    chisurf.logging.log(0, f'lineEdit_dragFile_injector::_dropEvent: {filepath}')
+                    self.target.append(filepath)
+        if url.scheme() == 'file':
+            filepath = str(url.toLocalFile())
+            filepath = pathlib.Path(filepath).as_posix()
+            self.lineEdit.setText(filepath)
+        if callable(self.call):
+            self.call()
