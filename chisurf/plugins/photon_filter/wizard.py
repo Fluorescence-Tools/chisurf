8b24a48eafb9095c93b1d8c28846f4402419a497
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-12 22:28:30 +0100

    Add photon filter wizard

---
+++ chisurf/plugins/photon_filter/wizard.py
@@ -0,0 +1,41 @@
+import sys
+from chisurf.gui import QtWidgets, QtGui, QtCore
+
+import chisurf.gui
+import chisurf.gui.widgets.wizard
+import chisurf.gui.widgets
+import chisurf.gui.decorators
+import chisurf.gui.tools
+import chisurf.gui.tools.parameter_editor
+
+import chisurf.data
+import chisurf.experiments
+import chisurf.curve
+import chisurf.fitting
+
+import chisurf.macros
+
+
+
+class ChisurfWizard(QtWidgets.QWizard):
+
+    # @chisurf.gui.decorators.init_with_ui("photon_filter/tttr_photon_filter.ui", chisurf.settings.plugin_path)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWizardStyle(QtWidgets.QWizard.ModernStyle)
+
+        # File format
+        page = chisurf.gui.widgets.wizard.WizardTTTRPhotonFilter()
+        self.addPage(page)
+
+
+if __name__ == "plugin":
+    wizard = ChisurfWizard()
+    wizard.show()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    wizard = ChisurfWizard()
+    wizard.show()
+    sys.exit(app.exec_())
+
