c4d648784069e779ac9466fd28643366ab2d81a1
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-12 22:20:10 +0100

    Cleanup

--- chisurf/plots/wr_plot.py
+++ chisurf/plots/wr_plot.py
@@ -10,36 +10,11 @@ color_scheme = chisurf.settings.colors
 
 
 class ResidualPlot(plotbase.Plot):
-    """
-    Started off as a plotting class to display TCSPC-data displaying the IRF,
-    the experimental data, the residuals
-    and the autocorrelation of the residuals. Now it is also used also for fcs-data.
-
-    In case the models is a :py:class:`~experiment.models.tcspc.LifetimeModel` it takes the irf and displays it:
-
-        irf = fit.models.convolve.irf
-        irf_y = irf.y
-
-    The models data and the weighted residuals are taken directly from the fit:
-
-        model_x, model_y = fit[:]
-        wres_y = fit.weighted_residuals
-
-    """
 
     name = "Residuals"
 
-    def __init__(
-            self,
-            fit: chisurf.fitting.fit.FitGroup,
-            *args,
-            **kwargs
-    ):
-        super().__init__(
-            *args,
-            fit=fit,
-            **kwargs
-        )
+    def __init__(self, fit: chisurf.fitting.fit.FitGroup, *args, **kwargs):
+        super().__init__(*args, fit=fit, **kwargs)
         self.data_x, self.data_y = None, None
 
         curves = list()
@@ -61,9 +36,6 @@ class ResidualPlot(plotbase.Plot):
         # Get parameters from plot-control
         fits = self.fit
         for ci, fi in zip(self.curves, fits):
-            w_res = fi.weighted_residuals
-            x = np.arange(len(w_res.y))
-            ci.setData(
-                x=x,
-                y=w_res.y
-            )
+            w_res = fi.model.weighted_residuals
+            x = np.arange(len(w_res))
+            ci.setData(x, w_res)
