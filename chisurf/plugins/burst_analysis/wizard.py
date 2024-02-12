83f30b3df6667b9c329e4652c1c52d6c5ae7bf54
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-12 23:08:12 +0100

    Add Burst wizard
    Not ready

---
+++ chisurf/plugins/burst_analysis/wizard.py
@@ -0,0 +1,45 @@
+import json
+import os.path
+import pathlib
+import typing
+import numpy as np
+
+from chisurf.gui import QtWidgets, QtGui, QtCore
+
+import chisurf.gui
+import chisurf.gui.widgets.wizard
+import chisurf.gui.widgets
+import chisurf.gui.decorators
+import chisurf.gui.tools
+import chisurf.gui.tools.parameter_editor
+
+import chisurf.data
+import chisurf.experiments
+import chisurf.curve
+import chisurf.fitting
+
+import chisurf.macros
+
+
+name = "Burst Analysis"
+
+
+class ChisurfWizard(QtWidgets.QWizard):
+
+    # @chisurf.gui.decorators.init_with_ui("burst_analysis/tttr_photon_filter.ui", chisurf.settings.plugin_path)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWizardStyle(QtWidgets.QWizard.ModernStyle)
+
+        # File format
+        page = chisurf.gui.widgets.wizard.WizardTTTRFileFormat()
+        self.addPage(page)
+
+        # Analysis channel definition
+        page = chisurf.gui.widgets.wizard.WizardTTTRChannelDefinition()
+        self.addPage(page)
+
+
+if __name__ == '__main__':
+    wizard = ChisurfWizard()
+    wizard.show()
