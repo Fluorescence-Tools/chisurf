f239fee482f44052a9ae7b6fb1f5e581c2eba8dd
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-02-12 22:47:59 +0100

    Add TCSPCTTTRReader

--- chisurf/experiments/tcspc.py
+++ chisurf/experiments/tcspc.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os.path
+import pathlib
+
 import chisurf.fio.fluorescence.tcspc
 from chisurf import typing
 
@@ -16,9 +19,7 @@ import chisurf.fio.fluorescence
 from chisurf.experiments import reader
 
 
-class TCSPCReader(
-    reader.ExperimentReader
-):
+class TCSPCReader(reader.ExperimentReader):
 
     def __init__(
             self,
@@ -33,6 +34,7 @@ class TCSPCReader(
             polarization: str = 'vm',
             use_header: bool = True,
             fit_area: float = None,
+            fit_start_fraction: float = None,
             fit_count_threshold: float = None,
             reading_routine: str = 'csv',
             *args,
@@ -40,25 +42,8 @@ class TCSPCReader(
     ):
         """
 
-        :param dt:
-        :param rep_rate:
-        :param is_jordi:
-        :param mode:
-        :param g_factor:
-        :param rebin:
-        :param matrix_columns:
-        :param skiprows:
-        :param polarization:
-        :param use_header:
-        :param fit_area:
-        :param fit_count_threshold:
-        :param reading_routine:
-        :param args:
-        :param kwargs:
-
         Example
         -------
-
         >>> import pylab as p
         >>> import chisurf.experiments
         >>> filename = "../test/data/tcspc/ibh_sample/Decay_577D.txt"
@@ -69,20 +54,20 @@ class TCSPCReader(
         >>> x = data.x
         >>> y = data.y
         >>> p.plot(x, y)
-
         """
         super().__init__(*args, **kwargs)
         if dt is None:
             dt = chisurf.settings.tcspc['dt']
         if g_factor is None:
-            g_factor = chisurf.settings.tcspc['g_factor']
+            g_factor = chisurf.settings.anisotropy['g_factor']
         if rep_rate is None:
             rep_rate = chisurf.settings.tcspc['rep_rate']
         if fit_area is None:
             fit_area = chisurf.settings.tcspc['fit_area']
         if fit_count_threshold is None:
             fit_count_threshold = chisurf.settings.tcspc['fit_count_threshold']
-
+        if fit_start_fraction is None:
+            fit_start_fraction = chisurf.settings.tcspc['fit_start_fraction']
         self.dt = dt
         self.excitation_repetition_rate = rep_rate
         self.is_jordi = is_jordi
@@ -97,25 +82,19 @@ class TCSPCReader(
         self.matrix_columns = matrix_columns
         self.fit_area = fit_area
         self.fit_count_threshold = fit_count_threshold
+        self.fit_start_fraction = fit_start_fraction
         self.reading_routine = reading_routine
 
-    def autofitrange(
-            self,
-            data,
-            **kwargs
-    ) -> typing.Tuple[int, int]:
+    def autofitrange(self, data, **kwargs) -> typing.Tuple[int, int]:
         return chisurf.fluorescence.tcspc.initial_fit_range(
             data.y,
             self.fit_count_threshold,
-            self.fit_area
+            self.fit_area,
+            start_fraction=self.fit_start_fraction,
+            verbose=chisurf.verbose
         )
 
-    def read(
-            self,
-            filename: str = None,
-            *args,
-            **kwargs
-    ) -> chisurf.data.DataCurveGroup:
+    def read(self, filename: str = None, *args, **kwargs) -> chisurf.data.DataCurveGroup:
         if self.reading_routine == 'csv':
             data_group: chisurf.data.DataCurveGroup = chisurf.fio.fluorescence.tcspc.read_tcspc_csv(
                 filename=filename,
@@ -150,9 +129,9 @@ class TCSPCReader(
         return data_group
 
 
-class TCSPCSetupDummy(TCSPCReader):
+class TCSPCSimulatorSetup(TCSPCReader):
 
-    name = "Dummy-TCSPC"
+    name = "TCSPC-Simulator"
 
     def __init__(
             self,
@@ -170,25 +149,19 @@ class TCSPCSetupDummy(TCSPCReader):
         self.experiment = kwargs.get('experiment', None)
         if lifetime_spectrum:
             t = ','.join([str(x) for x in lifetime_spectrum])
-            self.lineEdit_2.setText(t)
+            self.controller.lineEdit_2.setText(t)
         self.instrument_response_function = instrument_response_function
         self.sample_name = sample_name
-        self.lifetime_spectrum = np.array(
-            lifetime_spectrum,
-            dtype=np.float64
-        )
+        self.lifetime_spectrum = np.array(lifetime_spectrum, dtype=np.float64)
         self.n_tac = n_tac
         self.dt = dt
         self.p0 = p0
         self.rep_rate = rep_rate
 
-    def read(
-            self,
-            filename: str = None,
-            *args, **kwargs
-    ) -> chisurf.data.DataCurveGroup:
+    def read(self, filename: str = None, *args, **kwargs) -> chisurf.data.DataCurveGroup:
         if filename is None:
             filename = self.sample_name
+        name = kwargs.get('name', filename)
         x = np.arange(self.n_tac) * self.dt
         time_axis, y = chisurf.fluorescence.general.calculate_fluorescence_decay(
             lifetime_spectrum=self.lifetime_spectrum,
@@ -199,12 +172,62 @@ class TCSPCSetupDummy(TCSPCReader):
             y=y,
             ey=chisurf.fluorescence.tcspc.counting_noise(y),
             setup=self,
-            name=filename,
+            name=name,
             experiment=self.experiment
         )
-        data_set.setup = self
         return chisurf.data.DataCurveGroup(
             [data_set],
             experiment=self.experiment,
             data_reader=self
         )
+
+
+class TCSPCTTTRReader(TCSPCReader):
+
+    def read(self, filename: str = None, *args, **kwargs) \
+            -> chisurf.data.DataCurveGroup:
+        import tttrlib
+        if os.path.isfile(filename):
+            tttr = tttrlib.TTTR(filename, self.reading_routine)
+            tttr_selected = tttr.get_tttr_by_channel(self.channel_numbers)
+            y, x = tttr_selected.get_microtime_histogram(self.micro_time_coarsening)
+            x *= 1.0e9  # decays in ns
+
+            y_pos = np.where(y > 0)[0]
+            i_y_max = y_pos[-1]
+            y = y[:i_y_max]
+            t = x[:i_y_max]
+
+            # Prepare name
+            fn, _ = os.path.splitext(filename)
+            name = fn + "_ch("
+            name += ",".join([str(i) for i in self.channel_numbers])
+            name += ")"
+
+            data_set = chisurf.data.DataCurve(
+                x=t, y=y, name=name,
+                experiment=self.experiment,
+                data_reader=self,
+                ey=chisurf.fluorescence.tcspc.counting_noise(y)
+            )
+            data_group = chisurf.data.DataGroup([data_set])
+            return data_group
+        else:
+            raise FileNotFoundError
+
+    def __int__(
+            self,
+            tttr_filename: pathlib.Path,
+            channel_numbers=None,
+            reading_routine: str = 'PTU',
+            micro_time_coarsening: int = 1,
+            *args,
+            **kwargs
+    ):
+        super().__int__(*args, **kwargs)
+        if channel_numbers is None:
+            channel_numbers = []
+        self.reading_routine = reading_routine
+        self.channel_numbers = channel_numbers
+        self.micro_time_coarsening = micro_time_coarsening
+
