036bed94a1c8fa87559e1d768109e27a12a15318
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-03-28 21:56:03 +0100

    Cleanup

--- chisurf/macros/model_parse.py
+++ chisurf/macros/model_parse.py
@@ -3,16 +3,11 @@ import chisurf
 import chisurf.experiments
 
 
-def change_model(
-        function_str: str
-) -> None:
-    """
-
-    :param function_str:
-    :return:
-    """
+def change_model(function_str: str, fit_idx: int = None) -> None:
     cs = chisurf.cs
-    fit = cs.current_fit
+    if fit_idx is None:
+        fit = cs.current_fit
+    else:
+        fit = chisurf.fits[fit_idx]
     for f in fit:
-        f.model.func = "%s" % function_str
-    # fit.model.parse.onEquationChanged()
+        f.model.func = f"{function_str}"
