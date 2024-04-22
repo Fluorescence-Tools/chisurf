ae4670da142b17a1c4e2ba0f9bcc5df1ea06bbf2
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-04-22 12:10:30 +0200

    Generalize distribution plot
    Now distribution plot can be
    added to all models. Skip distributions
    that are not attribute

--- chisurf/plots/distribution.py
+++ chisurf/plots/distribution.py
@@ -14,11 +14,62 @@ color_scheme = chisurf.settings.colors
 lw = plot_settings['line_width']
 
 
+# Define some distribution options: If an attribute in a model is there it will be plotted.
+# accessor: a function that accesses the attribute and returns a pair (y, x)
+# that is plotted
+# accessor_kwargs: are kwargs that are passed to the accessor function
+# plot_options: default options used for plotting
+distribution_options = {
+    'Distance': {
+        'attribute': 'distance_distribution',
+        'accessor': lambda x: (x[0][0], x[0][1]),
+        'accessor_kwargs': {},
+        'plot_options': {
+            'stepMode': False, #'right',
+            'connect': False, #'all',
+            'symbol': "t"
+        }
+    },
+    'FRET-rate': {
+        'attribute': 'fret_rate_spectrum',
+        'accessor': chisurf.math.datatools.interleaved_to_two_columns,
+        'accessor_kwargs': {'sort': True},
+        'plot_options': {
+            'stepMode': False, #'right',
+            'connect': False, # 'all',
+            'symbol': "x"
+        }
+    },
+    'Lifetime': {
+        'attribute': 'lifetime_spectrum',
+        'accessor': chisurf.math.datatools.interleaved_to_two_columns,
+        'accessor_kwargs': {'sort': True},
+        'plot_options': {
+            'stepMode': False,
+            'connect': False,
+            'symbol': "o"
+        }
+    }
+}
+
+
 class DistributionPlotControl(QtWidgets.QWidget):
 
     @property
     def distribution_type(self):
         return str(self.selector.currentText())
+
+    def add_distribution_choices(self):
+        model = self.parent().fit.model
+        items = list()
+        for distribution_type in distribution_options.keys():
+            d = distribution_options[distribution_type]
+            try:
+                attr = model.__getattribute__(d['attribute'])
+                items.append(distribution_type)
+            except AttributeError:
+                pass
+        self.selector.addItems(items)
 
     def __init__(
             self,
@@ -30,15 +81,11 @@ class DistributionPlotControl(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout()
         self.setLayout(self.layout)
         self.selector = QtWidgets.QComboBox(None)
+        self.selector.blockSignals(True)
         self.layout.addWidget(self.selector)
-        self.selector.addItems(
-            [
-                'Distance',
-                'Lifetime',
-                'FRET-rate'
-            ]
-        )
+        self.add_distribution_choices()
         self.selector.currentIndexChanged[int].connect(parent.update)
+        self.selector.blockSignals(False)
 
 
 class DistributionPlot(plotbase.Plot):
@@ -75,20 +122,6 @@ class DistributionPlot(plotbase.Plot):
 
     def update(self, *args, **kwargs) -> None:
         super().update(*args, **kwargs)
-        if self.plot_controller.distribution_type == 'Distance':
-            d = self.fit.model.distance_distribution
-            y = d[0][0]
-            x = d[0][1]
-        elif self.plot_controller.distribution_type == 'FRET-rate':
-            lifetime_spectrum = self.fit.model.fret_rate_spectrum
-            y, x = chisurf.math.datatools.interleaved_to_two_columns(
-                lifetime_spectrum,
-                sort=True
-            )
-        else: #elif self.plot_controller.distribution_type == 'Lifetime':
-            lifetime_spectrum = self.fit.model.lifetime_spectrum
-            y, x = chisurf.math.datatools.interleaved_to_two_columns(
-                lifetime_spectrum,
-                sort=True
-            )
-        self.distribution_curve.setData(x, y)
+        d = distribution_options[self.plot_controller.distribution_type]
+        y, x = d['accessor'](self.fit.model.__getattribute__(d['attribute']), **d['accessor_kwargs'])
+        self.distribution_curve.setData(x, y, **d['plot_options'])
