1e72d346df396772ded36833379ef717580dbf7f
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-05-07 08:19:26 +0200

    Add WLC plot

---
+++ manual/plots/rdf-wlc.py
@@ -0,0 +1,13 @@
+import chisurf.math.functions.rdf as rdf
+import matplotlib.pyplot as p
+import numpy as np
+
+r = np.linspace(0, 0.99, 200)
+
+p.plot(r, rdf.worm_like_chain(r, 1.0))
+p.plot(r, rdf.worm_like_chain(r, 0.8))
+p.plot(r, rdf.worm_like_chain(r, 0.6))
+p.plot(r, rdf.worm_like_chain(r, 0.4))
+p.plot(r, rdf.worm_like_chain(r, 0.2))
+
+p.show()
