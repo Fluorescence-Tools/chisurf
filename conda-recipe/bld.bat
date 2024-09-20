e398453df0f28cc7f1a144885e830a98540e3ff3
tpeulen <thomas.peulen@posteo.de>
2024-09-20 23:35:22 +0200

    Fix CI

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-"%BUILD_PREFIX%"\pyrcc5.bat chisurf/gui/resources/resource.qrc -o chisurf/gui/resources/resource.py
-"%PYTHON%" setup.py build_ext --force --inplace
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+rem "%BUILD_PREFIX%"\pyrcc5.bat chisurf/gui/resources/resource.qrc -o chisurf/gui/resources/resource.py
+python setup.py build_ext --force --inplace
+python setup.py install --single-version-externally-managed --record=record.txt
