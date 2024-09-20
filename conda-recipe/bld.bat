f5c286fd353a85223dc07183cd127d282ad56ccc
tpeulen <thomas.peulen@posteo.de>
2024-09-20 21:13:10 +0200

    Fix CI
    * Fix changed location of pyrcc5.bat
    * run windows build without headless gui

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-call "%BUILD_PREFIX%"\Library\bin\pyrcc5.bat chisurf/gui/resources/resource.qrc -o chisurf/gui/resources/resource.py
+call "%BUILD_PREFIX%"\pyrcc5.bat chisurf/gui/resources/resource.qrc -o chisurf/gui/resources/resource.py
 "%PYTHON%" setup.py build_ext --force --inplace
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
