525fd528411cb852bf247946133ac05cc129e1e4
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-10-09 10:58:26 +0200

    Enhancement
    * Display error message in case
    the data cannot be imported

--- chisurf/macros/core_data.py
+++ chisurf/macros/core_data.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import traceback
 
 import chisurf.base
 import chisurf.data
 import chisurf.fitting
 import chisurf.gui
+import chisurf.gui.widgets
+
 from chisurf import typing
 
 
@@ -63,32 +66,39 @@ def add_dataset(
         dataset: chisurf.base.Data = None,
         **kwargs
 ) -> None:
-    cs = chisurf.cs
+    try:
+        cs = chisurf.cs
 
-    # Handle file name argument from prompt
-    # if multiple filenames (list) cleanup list else
-    # use only one filename (old behaviour)
-    filename = kwargs.get('filename', None)
-    if filename is not None:
-        filename = filename.split('|')
-        if len(filename) == 1:
-            filename = filename[0]
-    kwargs['filename'] = filename
+        filename = kwargs.get('filename', None)
+        if filename is not None:
+            filename = filename.split('|')
+            if len(filename) == 1:
+                filename = filename[0]
+        kwargs['filename'] = filename
 
-    if expriment_reader is None:
-        expriment_reader = cs.current_experiment_reader
-    if dataset is None:
-        dataset = expriment_reader.get_data(**kwargs)
+        if expriment_reader is None:
+            expriment_reader = cs.current_experiment_reader
+        if dataset is None:
+            dataset = expriment_reader.get_data(**kwargs)
 
-    if isinstance(dataset, chisurf.data.ExperimentDataGroup):
-        dataset_group = dataset
-    else:
-        dataset_group = chisurf.data.ExperimentDataCurveGroup(dataset)
+        if isinstance(dataset, chisurf.data.ExperimentDataGroup):
+            dataset_group = dataset
+        else:
+            dataset_group = chisurf.data.ExperimentDataCurveGroup(dataset)
 
-    if len(dataset_group) == 1:
-        chisurf.imported_datasets.append(dataset_group[0])
-    else:
-        chisurf.imported_datasets.append(dataset_group)
+        if len(dataset_group) == 1:
+            chisurf.imported_datasets.append(dataset_group[0])
+        else:
+            chisurf.imported_datasets.append(dataset_group)
 
-    cs.update()
+        cs.update()
 
+    except Exception as e:
+        # Capture the full error trace
+        error_trace = traceback.format_exc()
+        # Show the error popup
+        chisurf.gui.widgets.msg_box = chisurf.gui.widgets.MyMessageBox(
+            label="Error",
+            info="Error reading data. Check Reading settings and file.",
+            details=error_trace
+        )
