ddae5df8a38e4bc8162708f3853b42ede26dfc86
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-10-11 14:42:21 +0200

    Cleanup

---
+++ chisurf/plugins/_photon_filter.old/wizard.py
@@ -0,0 +1,45 @@
+import sys
+from chisurf.gui import QtWidgets, QtGui, QtCore
+
+import chisurf.gui
+import chisurf.gui.widgets.wizard
+import chisurf.gui.widgets
+import chisurf.gui.decorators
+import chisurf.gui.tools
+import chisurf.gui.tools.parameter_editor
+
+import chisurf.data
+import chisurf.experiments
+import chisurf.curve
+import chisurf.fitting
+
+import chisurf.macros
+
+
+
+class ChisurfWizard(QtWidgets.QWizard):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWizardStyle(QtWidgets.QWizard.ModernStyle)
+
+        # File format
+        self.filter = chisurf.gui.widgets.wizard.WizardTTTRPhotonFilter()
+        self.addPage(self.filter)
+        self.button(QtWidgets.QWizard.FinishButton).clicked.connect(self.onFinish)
+
+    def onFinish(self):
+        print("Saving photon selection")
+        self.filter.save_filter_data()
+
+
+if __name__ == "plugin":
+    wizard = ChisurfWizard()
+    wizard.show()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    wizard = ChisurfWizard()
+    wizard.show()
+    sys.exit(app.exec_())
+
