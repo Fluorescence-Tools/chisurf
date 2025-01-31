41b57171f5cc2466c901166776be7ee5fbd93613
tpeulen <thomas.peulen@posteo.de>
2025-01-31 08:44:53 +0100

    Fix windows build.

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -43,7 +43,7 @@ if exist build rmdir /s /q build
 mkdir build
 cd build
 :: Configure the build using CMake
-cmake .. -G "Visual Studio 15 2017" -A x64 ^
+cmake .. -G "Visual Studio 17 2022" -A x64 ^
  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
  -DCMAKE_PREFIX_PATH="%PREFIX%" ^
  -DBUILD_PYTHON_INTERFACE=ON ^
@@ -60,7 +60,7 @@ cmake .. -G "Visual Studio 15 2017" -A x64 ^
 cmake --build . --config Release --target install
 cd ..\..\..
 
-:: Build fit2x module (same as build.sh)
+:: Build fit2x module
 cd modules\fit2x
 git switch master
 if exist build rmdir /s /q build
@@ -72,14 +72,22 @@ for /f "tokens=2 delims= " %%v in ('%PYTHON% --version 2^>^&1') do set PYTHON_VE
 :: Extract only the numeric part of the version
 for /f "tokens=1-3 delims=." %%a in ("%PYTHON_VERSION%") do set PYTHON_VERSION_NUMERIC=%%a.%%b.%%c
 
-cmake .. -G "Visual Studio 15 2017" -A x64 ^
+REM Configure the build using CMake
+cmake .. -G "Visual Studio 17 2022" ^
  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
  -DCMAKE_PREFIX_PATH="%PREFIX%" ^
  -DBUILD_PYTHON_INTERFACE=ON ^
  -DCMAKE_BUILD_TYPE=Release ^
+ -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="%SP_DIR%" ^
  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="%SP_DIR%" ^
- -DPYTHON_VERSION="%PYTHON_VERSION_NUMERIC%" ^
- -DCMAKE_SWIG_OUTDIR="%SP_DIR%"
+ -DCMAKE_SWIG_OUTDIR="%SP_DIR%" ^
+ -DPython_ROOT_DIR="%PREFIX%\bin" ^
+ -DBUILD_LIBRARY=OFF ^
+ -DBUILD_PYTHON_DOCS=ON ^
+ -DWITH_AVX=OFF ^
+ -Wno-dev ^
+ -DBoost_USE_STATIC_LIBS=OFF
+
 :: Build and install the project
 cmake --build . --config Release --target install
 cd ..\..\..
