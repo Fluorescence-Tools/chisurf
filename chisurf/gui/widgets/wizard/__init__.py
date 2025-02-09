72288fe99e307284a0ca3f81dde0981a700bf92e
tpeulen <thomas.peulen@posteo.de>
2025-02-09 09:45:22 +0100

    Address photon filter issue

--- chisurf/gui/widgets/wizard/__init__.py
+++ chisurf/gui/widgets/wizard/__init__.py
@@ -1,5 +1,5 @@
 from . tttr_file_format import *
 from . tttr_channel_definition import *
 from . tttr_correlator import *
-from . tttr_photon_filter import *
+from . tttr_burst_finder import *
 from . fcs_merger import *
