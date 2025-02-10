422e4c66dfb79889c0a0b5031402881144b8cb5f
tpeulen <thomas.peulen@posteo.de>
2025-02-10 22:20:13 +0100

    Add missing import

--- chisurf/gui/widgets/wizard/__init__.py
+++ chisurf/gui/widgets/wizard/__init__.py
@@ -2,4 +2,5 @@ from . tttr_file_format import *
 from . tttr_channel_definition import *
 from . tttr_correlator import *
 from . tttr_burst_finder import *
+from . tttr_photon_filter import *
 from . fcs_merger import *
