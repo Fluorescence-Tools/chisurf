14632d2604ec10c35868d812d1f127aeca61de67
tpeulen <thomas.peulen@posteo.de>
2025-05-15 18:08:45 +0200

    Move FRET calculator and k2 dist to plugins

--- chisurf/gui/tools/__init__.py
+++ chisurf/gui/tools/__init__.py
@@ -3,9 +3,7 @@ This module contains a collection of tools
 """
 
 import chisurf.gui.tools.tttr
-import chisurf.gui.tools.fret
 import chisurf.gui.tools.structure
 import chisurf.gui.tools.code_editor
 import chisurf.gui.tools.f_test
 import chisurf.gui.tools.parameter_editor
-import k2dist as kappa2_distribution
