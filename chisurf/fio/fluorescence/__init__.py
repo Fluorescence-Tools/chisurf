15464c08da86be686b1a1a67c66d94142d26f7f1
tpeulen <thomas.peulen@posteo.de>
2025-08-04 00:47:46 +0200

    Replace `sdtfile` import with `bhfiles` in fluorescence module

--- chisurf/fio/fluorescence/__init__.py
+++ chisurf/fio/fluorescence/__init__.py
@@ -1,4 +1,4 @@
 import chisurf.fio.fluorescence.fcs
 import chisurf.fio.fluorescence.photons
-import chisurf.fio.fluorescence.sdtfile
 import chisurf.fio.fluorescence.burst
+import chisurf.fio.fluorescence.bhfiles
