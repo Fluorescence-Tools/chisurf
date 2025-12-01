de96451b47d1deaf99a326bb5b2b9e1793130eb6
tpeulen <thomas.peulen@posteo.de>
2025-12-02 00:41:59 +0100

    Refactor

--- chisurf/gui/widgets/experiments/tcspc/controller.py
+++ chisurf/gui/widgets/experiments/tcspc/controller.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-Rfrom .csv_tcspc_widget import CsvTCSPCWidget
+from .csv_tcspc_widget import CsvTCSPCWidget
 from .tcspc_reader_control_widget import TCSPCReaderControlWidget
 from .tcspc_tttr_reader_control_widget import TCSPCTTTRReaderControlWidget
 from .tcspc_simulator_setup_widget import TCSPCSimulatorSetupWidget
