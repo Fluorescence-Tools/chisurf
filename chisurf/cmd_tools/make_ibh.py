cb6fa92619d6c1e6596bc6ed3e0f68283eef9b13
tpeulen <thomas.peulen@posteo.de>
2025-12-09 17:40:56 +0100

    Remove old entry points

--- chisurf/cmd_tools/make_ibh.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Thomas'
-# Jordi2IBH
-# tttr 2 ibh
-# PQ 2 ibh
-# SDT 2 ibh
--- chisurf/entry_points/__init__.py
+++
@@ -1,3 +0,0 @@
-# This module is intentionally empty.
-# It exists to make 'chisurf.entry_points' a regular package
-# so setuptools package discovery is unambiguous.
