e86e797930a2bda9e39bfb1a94d55869395ce5df
tpeulen <thomas.peulen@posteo.de>
2025-12-09 18:03:29 +0100

    Remove dye sim models (all in quest now).

--- chisurf/models/tcspc/__init__.py
+++ chisurf/models/tcspc/__init__.py
@@ -2,4 +2,3 @@ import chisurf.models.tcspc.lifetime
 import chisurf.models.tcspc.fret
 import chisurf.models.tcspc.pddem
 import chisurf.models.tcspc.widgets
-import chisurf.models.tcspc.dye_diffusion
