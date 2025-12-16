cd9115e5a935dc63c844217f9baf2d666b56e6ee
tpeulen <thomas.peulen@posteo.de>
2025-12-16 10:11:13 +0100

    Add entry point for `CodeEditor` plugin with GUI initialization in `__main__.py`.

---
+++ chisurf/plugins/misc/code_editor/__main__.py
@@ -0,0 +1,15 @@
+import sys
+
+from chisurf.gui import QtWidgets
+from chisurf.plugins.misc.code_editor import CodeEditor
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    win = CodeEditor()
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
