5f954540a3c6dbfe85d4bf4b7c497f543634cc25
tpeulen <thomas.peulen@posteo.de>
2025-12-19 08:43:04 +0100

    Expose `cli_only` in `fcs_convert` module and update `__all__`.

--- chisurf/plugins/fcs/fcs_convert/__init__.py
+++ chisurf/plugins/fcs/fcs_convert/__init__.py
@@ -8,5 +8,6 @@ name = "Fluorescence Correlation Spectroscopy:FCS Converter"
 
 # Expose the Click CLI through the top-level ``csc`` entry point.
 cli_entrypoint = "fcs-convert=chisurf.plugins.fcs.fcs_convert.cli:cli"
+cli_only = True
 
-__all__ = ["name", "cli_entrypoint"]
+__all__ = ["name", "cli_entrypoint", "cli_only"]
