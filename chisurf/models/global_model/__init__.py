c953d99d01866a47161f4225f1d9fe92138d49b9
tpeulen <thomas.peulen@posteo.de>
2025-12-19 08:44:54 +0100

    Remove `agentfit` and `agentfit_widget` imports from `chisurf.models.global_model`: streamline module structure.

--- chisurf/models/global_model/__init__.py
+++ chisurf/models/global_model/__init__.py
@@ -1,8 +1,6 @@
 """
 Global model module for chisurf.
-Contains models for global fitting, including the agent-based fitter.
+Contains models for global fitting.
 """
 from . globalfit import *
 from .widget import *
-from .agentfit import *
-from .agentfit_widget import *
