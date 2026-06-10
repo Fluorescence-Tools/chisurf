ba6903d07d725557532d68713f4ab572cbbbc201
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2026-06-10 19:27:44 +0200

    chore: update configuration files

--- setup.py
+++ setup.py
@@ -1,42 +1,13 @@
 import os
 import sys
 import pathlib
-from setuptools import setup, Extension, find_packages
-from Cython.Build import cythonize
-import numpy as np
-from setuptools.command.build_py import build_py as _build_py
+from setuptools import setup, find_packages
 
-class CustomBuildPy(_build_py):
-    """Custom build_py to ensure extensions are built."""
-    def run(self):
-        self.run_command('build_ext')
-        return super().run()
-
-# Define extension modules
-extensions = [
-    Extension(
-        "chisurf.core.structure.av.fps_",
-        sources=[
-            "chisurf/core/structure/av/fps_.pyx",
-            "chisurf/core/structure/av/mt19937cok.cpp",
-        ],
-        include_dirs=[np.get_include()],
-        language="c++",
-    ),
-    Extension(
-        "chisurf.core.structure.potential.cPotentials_",
-        sources=["chisurf/core/structure/potential/cPotentials_.pyx"],
-        include_dirs=[np.get_include()],
-        language="c++",
-    ),
-]
 
 setup(
     name="chisurf",
     version=os.environ.get('CHISURF_VERSION', '26.dev0'),
     packages=find_packages(),
-    ext_modules=cythonize(extensions, language_level=3),
     include_package_data=True,
     zip_safe=False,
-    cmdclass={'build_py': CustomBuildPy},
 )
