90605f258f01c6fc5dbcbac1e1f83c1705f2d6e5
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2026-07-01 07:58:45 +0200

    fix(wizard): drop import of deleted tttr_burstfinder module
    Commit adea9754 removed the chisurf/gui/widgets/wizard/tttr_burstfinder
    package but left its wildcard re-export in wizard/__init__.py, breaking
    'import chisurf.gui.widgets.wizard' with ModuleNotFoundError. Remove the
    dangling import.

--- chisurf/gui/widgets/wizard/__init__.py
+++ chisurf/gui/widgets/wizard/__init__.py
@@ -1,6 +1,5 @@
 from .tttr_fileformat import *
 from .tttr_channeldefinition import *
 from .tttr_correlator import *
-from .tttr_burstfinder import *
 from .tttr_photonfilter import *
 from .fcs_merger import *
